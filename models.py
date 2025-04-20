01e8a4b7ec55979987c81cdb8286f6cdae8d16e9
Tim Sigafoos <tim@2gooddogs.com>
2025-04-20 00:54:39 -0700

    modify user table schema

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from sqlmodel import SQLModel, Field
 from typing import Optional
 
 class User(SQLModel, table=True):
+    __tablename__ = "users"  # Explicitly set the table name to "users"
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(index=True, unique=True)
     email: str = Field(index=True, unique=True)
